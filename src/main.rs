f69e526533a0c936807ac20f7802894e4822aaec
Chandan Bangalore Shankar Denanath <49138440+chandanbsd@users.noreply.github.com>
2024-12-14 10:56:05 -0500

    Add gitignore to prevent build outputs from being checked in

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
 }
+
+fn main() {
+
+    let deck = Deck {
+        cards: Vec::new(),
+    };
+
+    let a = deck.cards.len();
+
+    println!("The number of cards in the current deck is: {:?}", a);
+    println!("The deck: {:?}", deck);
+
+}
+ 
