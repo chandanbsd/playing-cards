61f8c51906016ab4b1cff1ae7c77957d2bc6968c
chandanbsd <49138440+chandanbsd@users.noreply.github.com>
2024-12-15 10:56:49 -0500

    Added suits, ranks, rank cards and shuffle capability

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,60 @@
+use std::fmt::format;
+
+use rand::{self, seq::{self, SliceRandom}, thread_rng};
+
 #[derive(Debug)]
 struct Deck {
     cards: Vec<String>,
 }
 
+impl Deck {
+
+    fn new() -> Self {
+        Deck {
+            cards: Deck::build_deck(),
+        }
+    }
+
+    fn build_deck() -> Vec<String>  {
+        let suits = ["Hearts", "Diamonds", "Spades", "Club"];
+        let rank_cards =["King", "Queen", "Joker"];
+        let rank_start = 1;
+        let rank_end = 13;
+
+        let mut cards: Vec<String> = Vec::new();
+
+        for suit in &suits {
+            for rank_card in &rank_cards {
+                cards.push(format!("{:#?} of {:#?}", rank_card, suit))
+            }
+            for rank in rank_start..=rank_end{
+                cards.push(format!("{:?} of {:?}", rank, suit))
+            }
+        }
+
+        cards
+    }
+
+    fn shuffle(&mut self) {
+        let mut rng = thread_rng();
+        self.cards.shuffle(&mut rng);
+    }
+    
+}
+
 fn main() {
 
-    let deck = Deck {
-        cards: Vec::new(),
-    };
+    let mut deck = Deck::new();
 
-    let a = deck.cards.len();
+    let mut len = deck.cards.len();
 
-    println!("The number of cards in the current deck is: {:?}", a);
+    println!("The number of cards in the current deck is: {:?}", len);
     println!("The deck: {:?}", deck);
+
+    deck.shuffle();
+    println!("Shuffling deck....\n Deck = {:#?}", deck);
+
+    Deck::build_deck();
 
 }
  
